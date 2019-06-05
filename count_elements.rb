430898a74fc946e054d728e5ce22106ee5ff75f3
Karen Go <karenrgo@gmail.com>
2019-06-04 17:40:17 -0700

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,7 @@
 def count_elements(array)
-  # code goes here
+  # returns a hash of k/v where the keys are the original string elements of the array and values are a count of how many times they appeared in the array
+  unique_keys = array.uniq
+  counts_values = unique_keys.map {|x| array.count(x)}
+  Hash[unique_keys.zip(counts_values)]
 end
  
