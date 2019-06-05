41e58de0414335fb718442790c234a37ec1dc178
github username <github email address>
2019-06-04 17:32:31 -0700

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,10 @@
 def count_elements(array)
   # code goes here
+  new_hash = Hash.new(0)
+  array.each { |name| new_hash[name] += 1 }
+  return new_hash
+  binding.pry
 end
- 
+# names = ["Jason", "Jason", "Teresa", "Judah", "Michelle", "Judah", "Judah", "Allison"]
+# counts = Hash.new(0)
+# names.each { |name| counts[name] += 1 }
